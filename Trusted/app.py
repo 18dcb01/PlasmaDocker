308bc6938f22c831e2cfb5e0df02ec1a1ddad87f
18dcb01 <danielbarnett5000000@gmail.com>
2019-07-02 11:16:07 -0700

    switched to grpc

--- Trusted/app.py
+++ Trusted/app.py
@@ -3,10 +3,13 @@ from redis import Redis, RedisError
 import pyarrow
 import pyarrow.plasma as plasma
 import pyarrow.parquet as pq
-import pandas
 import os
 import socket
 import time
+import random
+import grpc
+import codeRunner_pb2_grpc
+import codeRunner_pb2
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
@@ -17,101 +20,120 @@ app = Flask(__name__)
 def makeID(id_):
     return plasma.ObjectID(id_.encode("utf8"))
 
+def randString():
+    chars = """abcdefghijklmnopqr stuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()~`-_=+[{]};:'"/?>.<,|\\"""
+    return ''.join([random.choice(chars) for n in range(20)])
 
-keep = []
+
 
 
 @app.route("/")
 def hello():
-    client = plasma.connect("/tmp/plasma")
+    channel = grpc.insecure_channel('untrusted:50051')
+    stub = codeRunner_pb2_grpc.codeRunnerStub(channel)
 
-    id_ = 1000000000
-    while client.contains(makeID("trustclaim"+str(id_))):
-        id_ += 1
-
-    client.put(1, makeID("trustclaim" + str(id_)))
-    keep.append(client.get_buffers([makeID("trustclaim"+str(id_))]))
-    keep.append(client.get_buffers([makeID("claims on "+str(id_))]))
-
-    print(str(id_)+" claimed")
+    rand = True
 
     from pyarrow import csv
-    fn = "mimic.csv"
+    fn = "IRAhandle_tweets_1.csv" if rand else "mimic.csv"
     table = csv.read_csv(fn)
+    start = time.clock()
 
     print("data loaded")
 
     batches = table.to_batches()
+    print(1)
+    client = plasma.connect("/tmp/plasma")
 
-    strId = makeID("dataset id"+str(id_))
-
-    mock_sink = pyarrow.MockOutputStream()#find data size
-    stream_writer = pyarrow.RecordBatchStreamWriter(mock_sink, batches[0].schema)
-    for batch in batches:
-        stream_writer.write_batch(batch)
-    stream_writer.close()
-    data_size = mock_sink.size()
-
-    buf = client.create(strId, data_size)
-
-    stream = pyarrow.FixedSizeBufferWriter(buf)
-    stream_writer = pyarrow.RecordBatchStreamWriter(stream, batches[0].schema)
-    for batch in batches:
-        stream_writer.write_batch(batch)
-    stream_writer.close()
-
-    client.seal(strId)
-    print("data sent")
+    print(2)
 
     code = """
+while True:
+    i = 7
+
+
+
+""" if False else """
+
 import os
 import pyarrow
 import sys
-import pyarrow.plasma
 
-reader = pyarrow.RecordBatchStreamReader(sys.stdin.buffer)
-
-dataTable = reader.read_all()
-
+authors = dataTable.column("author")
+newData = []
+for i in range(len(authors)):
+    newData.append(1 if i == 0 or authors[i] != authors[i-1] else newData[-1]+1)
+newColumn = dataTable.column(3).from_array("authorTweetCount", [newData])
+newTable = dataTable.append_column(newColumn)
+#print(dataTable.schema)
+    """ if rand else"""
+import os
+import pyarrow
+import sys
 
 ages = dataTable.column("age")
 maxV = max(ages.to_pylist())
 newData = []
-for i in range(len(ages)):
-    newData.append(1 if ages[i] == maxV else 0)
-newColumn = dataTable.column(3).from_array("authorTweetCount", [newData])
+for i in ages:
+    newData.append(1 if i == maxV else 0)
+newColumn = dataTable.column(3).from_array("oldest", [newData])
 dataTable = dataTable.append_column(newColumn)
-
-batches = dataTable.to_batches()
-
-stream_writer = pyarrow.RecordBatchStreamWriter(sys.stdout.buffer, dataTable.schema)
-for batch in batches:
-    stream_writer.write_batch(batch)
-stream_writer.close()
     """
 
-    client.put(code, makeID("executable"+str(id_)))
-    print("executable sent")
+    tables = []
 
+    for i in range(len(batches)):
+        id_ = randString()
 
-    [data] = client.get_buffers([makeID("returnable"+str(id_))])
+        strId = makeID(id_)
+        
+        mock_sink = pyarrow.MockOutputStream()  #find data size
+        stream_writer = pyarrow.RecordBatchStreamWriter(mock_sink, batches[0].schema)
+        stream_writer.write_batch(batches[i])
+        stream_writer.close()
+        data_size = mock_sink.size()
+        buf = client.create(strId, data_size)
 
-    buffer_ = pyarrow.BufferReader(data)
-    reader = pyarrow.RecordBatchStreamReader(buffer_)
-    datatable = reader.read_all()
+        stream = pyarrow.FixedSizeBufferWriter(buf)
+        stream_writer = pyarrow.RecordBatchStreamWriter(stream, batches[0].schema)
+        stream_writer.write_batch(batches[i])
+        stream_writer.close()
 
-    html = str(datatable.column("authorTweetCount").data)
-    print("data recieved")
+        client.seal(strId)
+        print("sent batch "+str(i+1))
+        
+        codeToSend = codeRunner_pb2.code(toRun=code,id_=id_)
+        
+        newId = stub.runCode(codeToSend,timeout=1)
+        newId = newId.id_
+        
+        [data] = client.get_buffers([makeID(newId)])
+        outputBuf = pyarrow.py_buffer(data.to_pybytes())
+        buffer_ = pyarrow.BufferReader(outputBuf)
+        reader = pyarrow.RecordBatchStreamReader(buffer_)
+        if i == 0:
+            datatable = reader.read_all()
+        else:
+            datatable = pyarrow.concat_tables([datatable,datatable.from_batches(reader.read_all().to_batches())])
+
+    html = str(datatable.column("authorTweetCount" if rand else "oldest").data)
+    print("data received after " +str(time.clock()-start))
+
     return html
 
 
 if __name__ == "__main__":
     newpid = os.fork()
+    print(1)
 
-    if not newpid:#one path runs the plasma stores
+    if not newpid:  #one path runs the plasma stores
         import subprocess
-        subprocess.call(["plasma_store", "-m", "250000000", "-s", "/tmp/plasma"])
-        assert false #plasma store stopped?
+        print(2)
+        
+        subprocess.call(["plasma_store", "-m", "60000000", "-s", "/tmp/plasma"])
+        assert false  #plasma store stopped?
 
     else:
+        client = plasma.connect("/tmp/plasma")
+        
         app.run(host='0.0.0.0', port=80)
--- Untrusted/app.py
+++ Untrusted/app.py
@@ -1,9 +1,17 @@
+
+
+
+
+"""
 import pyarrow
 import pyarrow.plasma as plasma
 import os
 import socket
 import time
 import subprocess
+import signal
+import grpcio
+
 
 
 def makeID(id_):
@@ -14,58 +22,158 @@ def demote():
     os.setgid(65534)
     os.setuid(65534)
 
+def handler(signum, frame):
+    raise Exception("Timed out!")
+
 
 if __name__ == "__main__":
-    id_ = 1000000000
+    id_ = 10000
     keep = []
-    while True:
-        client = plasma.connect("/tmp/plasma")
-        if not client.contains(makeID("claims on " + str(id_))):
-            client.put(1, makeID("claims on " + str(id_)))
-            keep.append(client.get_buffers([makeID("claims on "+str(id_))]))
-            print(str(id_)+" claimed")
+    client = plasma.connect("/tmp/plasma")
+    while client.contains(makeID("untrusted claim" + str(id_))):
+        id_ += 1
+    client.put(1, makeID("untrusted claim" + str(id_)))
+    keep = client.get_buffers([makeID("untrusted claim"+str(id_))])
+    print(str(id_)+" claimed")
 
-            [data] = client.get_buffers([makeID("dataset id"+str(id_))])
+    numBuffers = client.get(makeID("trusted reserve" + str(id_)))
 
-            print("data recieved")
+    signal.signal(signal.SIGALRM, handler)
 
-            client.put(1, makeID("loaded id " + str(id_)))
-            keep=client.get_buffers([makeID("loaded id "+str(id_))])
+    for i in range(numBuffers):
+        
+        start = time.clock()
+        [data] = client.get_buffers([makeID("sent data "+str(id_)+str(i+10000))])
 
-            toExecute = ['python', '-c', client.get(makeID("executable" + str(id_)))]
+        print("data recieved")
 
-            sub = subprocess.Popen(toExecute, stdin=subprocess.PIPE, stdout=subprocess.PIPE, preexec_fn=demote)
-            print("sending data")
-            data, _ = sub.communicate(data)
-            print("process finished")
+        toExecute = ['python', '-c', client.get(makeID("user-made code " + str(id_)))]
 
-            buffer_ = pyarrow.BufferReader(data)
-            reader = pyarrow.RecordBatchStreamReader(buffer_)
+        sub = subprocess.Popen(toExecute, stdin=subprocess.PIPE, stdout=subprocess.PIPE, preexec_fn=demote)
+        print("sending data")
+        
+        signal.alarm(1)
+        try:
+            newData, _ = sub.communicate(data)
+        except:
+            newData = data
+            print("request timed out")
+        finally:
+            signal.alarm(0)
+        print("process finished")
 
-            dataTable = reader.read_all()
-            print("data recieved")
+        buffer_ = pyarrow.BufferReader(newData)
+        reader = pyarrow.RecordBatchStreamReader(buffer_)
 
-            batches = dataTable.to_batches()
+        dataTable = reader.read_all()
+        print("data recieved")
 
-            strId = makeID("returnable"+str(id_))
+        batches = dataTable.to_batches()
 
-            mock_sink = pyarrow.MockOutputStream()
-            stream_writer = pyarrow.RecordBatchStreamWriter(mock_sink, batches[0].schema)
-            for batch in batches:
-                stream_writer.write_batch(batch)
-            stream_writer.close()
-            data_size = mock_sink.size()
+        strId = makeID("to return "+str(id_)+str(i+10000))
 
-            buf = client.create(strId, data_size)
+        mock_sink = pyarrow.MockOutputStream()
+        stream_writer = pyarrow.RecordBatchStreamWriter(mock_sink, batches[0].schema)
+        for batch in batches:
+            stream_writer.write_batch(batch)
+        stream_writer.close()
+        data_size = mock_sink.size()
 
-            stream = pyarrow.FixedSizeBufferWriter(buf)
-            stream_writer = pyarrow.RecordBatchStreamWriter(stream, batches[0].schema)
-            for i, batch in enumerate(batches):
-                stream_writer.write_batch(batch)
-            stream_writer.close()
+        buf = client.create(strId, data_size)
 
-            client.seal(strId)
-            print("data sent")
-        else:
-            id_ += 1
-        client.disconnect()
+        stream = pyarrow.FixedSizeBufferWriter(buf)
+        stream_writer = pyarrow.RecordBatchStreamWriter(stream, batches[0].schema)
+        for j, batch in enumerate(batches):
+            print("returning " + str(j+1) + "/" +str(len(batches)))
+            stream_writer.write_batch(batch)
+        stream_writer.close()
+
+        buf = 0
+
+        client.seal(strId)
+        print(str(i+1)+"/"+str(numBuffers) + " in " +str(time.clock()-start))
+"""
+import pyarrow
+import pyarrow.plasma as plasma
+import os
+import socket
+import time
+import subprocess
+import signal
+import grpc
+import codeRunner_pb2_grpc
+import codeRunner_pb2
+import random
+from concurrent import futures
+
+
+
+def demote():
+    os.setgid(65534)
+    os.setuid(65534)
+
+def makeID(id_):
+    return plasma.ObjectID(id_.encode("utf8"))
+
+def randString():
+    chars = """abcdefghijklmnopqr stuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()~`-_=+[{]};:'"/?>.<,|\\"""
+    return ''.join([random.choice(chars) for n in range(20)])
+
+
+class codeRunnerServicer(codeRunner_pb2_grpc.codeRunnerServicer):
+    def __init__(self):
+        self.client = plasma.connect("/tmp/plasma")
+        demote()
+
+
+    def runCode(self, request, context):
+        start = time.clock()
+
+        [data] = self.client.get_buffers([makeID(request.id_)])
+        buffer_ = pyarrow.BufferReader(data)
+        reader = pyarrow.RecordBatchStreamReader(buffer_)
+        dataTable = reader.read_all()
+        
+        Vars = {'dataTable':dataTable}
+        exec(request.toRun,globals(),Vars)
+
+        dataTable = Vars.get('newTable')
+
+        batches = dataTable.to_batches()
+
+        id_ = randString()
+
+        mock_sink = pyarrow.MockOutputStream()
+        stream_writer = pyarrow.RecordBatchStreamWriter(mock_sink, batches[0].schema)
+        for batch in batches:
+            stream_writer.write_batch(batch)
+        stream_writer.close()
+        data_size = mock_sink.size()
+
+        buf = self.client.create(makeID(id_), data_size)
+
+        stream = pyarrow.FixedSizeBufferWriter(buf)
+        stream_writer = pyarrow.RecordBatchStreamWriter(stream, batches[0].schema)
+        for j, batch in enumerate(batches):
+            print("returning " + str(j+1) + "/" +str(len(batches)))
+            stream_writer.write_batch(batch)
+        stream_writer.close()
+
+        self.client.seal(makeID(id_))
+        print("time elapsed: " + str(time.clock()-start))
+        return codeRunner_pb2.id_(id_=id_)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers = 1))
+    codeRunner_pb2_grpc.add_codeRunnerServicer_to_server(
+    codeRunnerServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while(True):
+            time.sleep(5)
+    except:
+        server.stop(0)
+    
+if __name__ == "__main__":
+    serve()
